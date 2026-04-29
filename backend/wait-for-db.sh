875a191f354fe82e91000c214bfe271988e2ff25
ibro347 <ibromedmer@gmail.com>
2026-04-29 12:07:14 +0300

    switched to postgres

---
+++ backend/wait-for-db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+host="$1"
+port="$2"
+shift 2
+cmd="$@"
+
+until PGPASSWORD=$DB_PASSWORD psql -h "$host" -p "$port" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
